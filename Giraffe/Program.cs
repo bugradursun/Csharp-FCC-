56dd0a89deedc79ca05b4d505b2d821d520a8a02
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-09-25 01:09:19 +0300

    commit

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -8,14 +8,34 @@ namespace Giraffe
 {
     class Program
     {
+        public void Test()
+        {
+            Console.WriteLine("1st Method.");
+        }
+        public void Test(int i)
+        {
+            Console.WriteLine("2nd Method.");
+        }
+        public void Test(string s)
+        {
+            Console.WriteLine("3rd Method.");
+        }
+        public void Test(int i , string s)
+        {
+            Console.WriteLine("4th Method.");
+        }
+        public void Test(string s , int i)
+        {
+            Console.WriteLine("5th Method.");
+        }
+
         static void Main(string[] args)
         {
-            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
-            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
-            avengers.Rating = "Dog";
-            Console.WriteLine(avengers.Rating);
-
+            Program p = new Program();
+            p.Test("abc", 2);
+            p.Test(1);
             Console.ReadLine();
+      
         }
         
     }
