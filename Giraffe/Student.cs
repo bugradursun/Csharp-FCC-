77ddbe3281793fb3351c3a1e8f9444e5030a3109
Bugra Dursun <72981010+bugradursun@users.noreply.github.com>
2023-10-15 12:43:29 +0300

    Update Student.cs

--- Giraffe/Student.cs
+++ Giraffe/Student.cs
@@ -1,60 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Giraffe
+//using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace ExcelDataBase
 {
-    // we are sorting with interfaces in this example
-    //sorting mechanism in user defined types
-    public class Student : IComparable<Student>
+    public partial class Form1 : Form
     {
-        public int Sid { get; set; }
-        public string Name { get; set; } 
-        public int Class { get; set; }
-        public float Marks { get; set; }
 
-        public int CompareTo(Student other)
-        {
-            if (this.Sid > other.Sid)
-                return 1;
-            else if (this.Sid < other.Sid)
-                return -1;
-            else
-                return 0;
-        }
-    }
-    class CompareStudents : IComparer<Student>
-    {
-        public int Compare(Student x, Student y)
-        {
-            if (x.Marks > y.Marks)
-                return 1;
-            else if (x.Marks < y.Marks)
-                return -1;
-            else
-                return 0;
-        }
-    }
-    class TestStudent
-    {
-        static void Main()
-        {
-            Student s1 = new Student { Sid = 103, Name = "Bugra", Class = 10, Marks = 575.00f };
-            Student s2 = new Student { Sid = 105, Name = "Ahmet", Class = 10, Marks = 595.00f };
-            Student s3 = new Student { Sid = 101, Name = "Williams", Class = 10, Marks = 592.00f };
-            Student s4 = new Student { Sid = 108, Name = "David", Class = 10, Marks = 515.00f };
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-I376K2M;Initial Catalog=ProjelerVT;Integrated Security=True");
 
-            List<Student> Students = new List<Student>() { s1, s2, s3, s4 }; //adding custom defined user type(student) to our list
-            CompareStudents obj = new CompareStudents();
-            Students.Sort(obj);
-            foreach (Student S in Students)
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Excel.Application excelUygulama = new Excel.Application();
+            excelUygulama.Visible = true;
+            Excel.Workbook workbook = excelUygulama.Workbooks.Add(System.Reflection.Missing.Value); //workbook olusturduk
+            Excel.Worksheet sayfa1 = workbook.Sheets[1]; //worksheet olusturduk ve 1. index 0 degil 1 oluyor.!
+
+            string[] basliklar = { "Personel no", "Ad", "Soyad", "Semt", "Sehir" };
+            Excel.Range range;
+            for(int i = 0; i<basliklar.Length; i++)
             {
-                Console.WriteLine(S.Sid + "" + S.Name + "" + S.Class + S.Marks);
-
+                range = sayfa1.Cells[1, (1 + i)];
+                range.Value2 = basliklar[i];
             }
-            Console.ReadLine();
+
+
+            try
+            {
+                baglanti.Open();
+                string sqlCumlesi = "SELECT PersonelNo,Ad,Soyad,Semt,Sehir FROM Personel";
+                SqlCommand sqlCommand = new SqlCommand(sqlCumlesi, baglanti);
+                SqlDataReader sdr = sqlCommand.ExecuteReader(); //okuma islemni yapar
+                int satir = 2; //ilk satır baslikti,2. satırdan itibaren verileri dolduruyoruz
+                while(sdr.Read())
+                {
+                    string pno = sdr[0].ToString();
+                    string ad = sdr[1].ToString();
+                    string soyad = sdr[2].ToString();
+                    string semt = sdr[3].ToString();
+                    string sehir = sdr[4].ToString();
+
+                    richTextBox1.Text = richTextBox1.Text + pno + "" +  ad + "" + soyad + "" + semt + "" + sehir + "\n";
+
+                    range = sayfa1.Cells[satir,1];
+                    range.Value2 = pno;
+                    range = sayfa1.Cells[satir, 2];
+                    range.Value2 = ad;
+                    range = sayfa1.Cells[satir, 3];
+                    range.Value2 = soyad;
+                    range = sayfa1.Cells[satir, 4];
+                    range.Value2 = semt;
+                    range = sayfa1.Cells[satir, 5];
+                    range.Value2 = sehir;
+
+                    satir++;
+                }
+                
+            } catch(Exception ex) {
+                MessageBox.Show("SQL Query sırasında hata bulundu!, Hata Kodu:SQLREAD01 \n" + ex.ToString());
+            }
+            finally {
+                if(baglanti != null) //baglanti zaten var yok sorgula
+                    baglanti.Close();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
