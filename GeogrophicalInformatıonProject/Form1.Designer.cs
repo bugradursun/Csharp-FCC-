e78a4f3727b8319e01bdeee043c55f440722ae17
bugradursun <72981010+bugradursun@users.noreply.github.com>
2023-10-16 19:10:23 +0300

    commit

--- GeogrophicalInformatıonProject/Form1.Designer.cs
+++ GeogrophicalInformatıonProject/Form1.Designer.cs
@@ -28,13 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxEnlem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxBoylam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttton2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // map
+            // 
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(34, 1);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(822, 733);
+            this.map.TabIndex = 0;
+            this.map.Zoom = 0D;
+            this.map.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.map_OnMarkerClick);
+            this.map.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(981, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Haritada İsaretle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxEnlem
+            // 
+            this.textBoxEnlem.Location = new System.Drawing.Point(927, 73);
+            this.textBoxEnlem.Name = "textBoxEnlem";
+            this.textBoxEnlem.Size = new System.Drawing.Size(147, 22);
+            this.textBoxEnlem.TabIndex = 2;
+            this.textBoxEnlem.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(881, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enlem:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1080, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Boylam:";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // textBoxBoylam
+            // 
+            this.textBoxBoylam.Location = new System.Drawing.Point(1141, 73);
+            this.textBoxBoylam.Name = "textBoxBoylam";
+            this.textBoxBoylam.Size = new System.Drawing.Size(147, 22);
+            this.textBoxBoylam.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(947, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Virgul Kullanınız";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1157, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Virgul Kullanınız";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1138, 389);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Virgul Kullanınız";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(929, 389);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Virgul Kullanınız";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1061, 364);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Boylam:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1123, 364);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(862, 364);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 16);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Enlem:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(908, 364);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(962, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(10, 10);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Haritada İsaretle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // buttton2
+            // 
+            this.buttton2.Location = new System.Drawing.Point(978, 456);
+            this.buttton2.Name = "buttton2";
+            this.buttton2.Size = new System.Drawing.Size(256, 44);
+            this.buttton2.TabIndex = 16;
+            this.buttton2.Text = "Haritada İsaretle";
+            this.buttton2.UseVisualStyleBackColor = true;
+            this.buttton2.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(881, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 16);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Marker1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(881, 313);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 16);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Marker2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1321, 733);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.buttton2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxBoylam);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEnlem);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.map);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxEnlem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxBoylam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttton2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
--- GeogrophicalInformatıonProject/Form1.cs
+++ GeogrophicalInformatıonProject/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,121 @@ namespace GeogrophicalInformatıonProject
 {
     public partial class Form1 : Form
     {
+
+        GMapOverlay katman1;
         public Form1()
         {
             InitializeComponent();
+            initializeMap();
+        }
+
+        private void initializeMap()
+        {
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleMap;
+            map.Position = new GMap.NET.PointLatLng(36.0,42.0); //enlem ve boylam koordinatları (lattitude ve longitude)
+            map.Zoom = 4;
+            map.MinZoom = 3;
+            map.MaxZoom = 25;
+            katman1 = new GMapOverlay();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PointLatLng lokasyon1 = new PointLatLng(Convert.ToDouble(textBoxEnlem.Text),Convert.ToDouble(textBoxBoylam.Text));
+            GMarkerGoogle marker = new GMarkerGoogle(lokasyon1,GMarkerGoogleType.red_pushpin);
+            marker.ToolTipText = "Lokasyon1\n Tır\nTo İstanbul"; //markerın uzerine gelince 'Lokasyon1' gorunecek
+            marker.ToolTip.Fill = Brushes.Black;
+            marker.ToolTip.Foreground = Brushes.White;
+            marker.ToolTip.Stroke = Pens.Black; 
+            marker.ToolTip.TextPadding = new Size(10, 10);
+            marker.Tag = 101;
+
+            //.2.markeri olustuyurouz
+
+            GMarkerGoogle marker2 = new GMarkerGoogle(lokasyon1, GMarkerGoogleType.orange);
+            marker2.Tag = 102;
+
+            //Overlay(katman) olusturmamiz lazim
+            //Harita uzerinde goruntelenecek tum componentleri bu katman eklememiz gerekmekte
+
+            
+            //ilk olarak yeni olusturulan katmanı harita nesnesine eklemeliyiz
+            map.Overlays.Add(katman1);
+
+            //daha sonra marker(lar) ı eklemeliyiz
+            //ÖNCE OVERLAY(KATMAN) OLUSTURUP SONRA MARKER EKLİYORUZ !!
+
+            katman1.Markers.Add(marker);
+            katman1.Markers.Add(marker2);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            map.Dispose();
+            Application.Exit(); //uygulamadan cikip takili kalmamasini sagliyoruz
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void map_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            int markerId = (int)item.Tag;
+            Console.WriteLine("id:" + markerId +"olan markera tıklandı");
+
+           
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PointLatLng lokasyon2 = new PointLatLng(Convert.ToDouble(textBox2.Text), Convert.ToDouble(textBox1.Text));
+            GMarkerGoogle marker2 = new GMarkerGoogle(lokasyon2, GMarkerGoogleType.red_dot);
+            marker2.Tag = 102;
+            katman1.Markers.Add(marker2);
         }
     }
 }
